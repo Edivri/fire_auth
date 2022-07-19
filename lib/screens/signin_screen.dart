357fa41c925f5d089fc5cc9e505f679b2695158f
Ben Edivri <eddyjamin@gmail.com>
2022-07-19 03:00:32 +0100

    Formating

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -6,7 +6,7 @@ import 'package:fire_auth/utils/color_utils.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatefulWidget {
-  const SignInScreen({ Key? key }) : super(key: key);
+  const SignInScreen({Key? key}) : super(key: key);
 
   @override
   State<SignInScreen> createState() => _SignInScreenState();
@@ -22,55 +22,56 @@ class _SignInScreenState extends State<SignInScreen> {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            hexStringToColor("CB2B93"),
-            hexStringToColor("9546C4"),
-            hexStringToColor("5E61F4"),
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-          child: SingleChildScrollView(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("CB2B93"),
+          hexStringToColor("9546C4"),
+          hexStringToColor("5E61F4"),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-                child: Column(
-                children: <Widget>[
-                  logoWidget("images/logo.png"),
-                  // ignore: prefer_const_constructors
-                  SizedBox(
-                    height: 30,
-                  ),
-                  reusableTextField("Enter Username", Icons.person_outline, false, _emailTextController),
-                     SizedBox(
-                    height: 30,
-                  ),
-                  reusableTextField("Enter Password", Icons.lock_outline, false, _passwordTextController),
-                     SizedBox(
-                    height: 20,
-                  ),
-                  signInSignUpButton(context, true, (){
-
-                  })
-
-                ]),)),),
-        
-      
+          padding: EdgeInsets.fromLTRB(
+              20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+          child: Column(children: <Widget>[
+            logoWidget("images/logo.png"),
+            // ignore: prefer_const_constructors
+            SizedBox(
+              height: 30,
+            ),
+            reusableTextField("Enter Username", Icons.person_outline, false,
+                _emailTextController),
+            SizedBox(
+              height: 30,
+            ),
+            reusableTextField("Enter Password", Icons.lock_outline, false,
+                _passwordTextController),
+            SizedBox(
+              height: 20,
+            ),
+            signInSignUpButton(context, true, () {}),
+            signUpOption()
+          ]),
+        )),
+      ),
     );
   }
- Row signUpOption(){
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      const Text("Don't have account?",
-      style: TextStyle(color: Colors.white70)),
-      GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen()));
-        },
-        child: const Text(
-          "Sign Up",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-      )
-    ],
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have account?",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
     );
- }
+  }
 }
