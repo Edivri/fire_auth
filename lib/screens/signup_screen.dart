eb648b080280a17ff8a825a2a61ec68e7157dfc2
Ben Edivri <eddyjamin@gmail.com>
2022-07-19 03:48:33 +0100

    Update signup_screen.dart

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,17 +1,72 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:fire_auth/reusable_widgets/reusable_widget.dart';
+import 'package:fire_auth/screens/home_screen.dart';
+import 'package:fire_auth/utils/color_utils.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({ Key? key }) : super(key: key);
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _passwordTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+  final TextEditingController _userNameTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Sign Up",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("CB2B93"),
+            hexStringToColor("9546C4"),
+            hexStringToColor("5E61F4"),
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+              child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Username", Icons.person_outline, false,
+                    _userNameTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Email Id", Icons.person_outline, false,
+                    _emailTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Username", Icons.lock_outlined, false,
+                    _passwordTextController),
+                SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, false, () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                })
+              ],
+            ),
+          ))),
     );
   }
 }
